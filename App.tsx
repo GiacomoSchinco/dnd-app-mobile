b323d706028768884821dab514d03f6f251fc412
GiacomoSchinco <84517547+GiacomoSchinco@users.noreply.github.com>
2026-06-17 16:49:57 +0200

    default theme

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { PrismProvider } from './components/ui/prism-provider';
 import AppNavigator from './components/custom/AppNavigator';
 
 // Scegli il tema che preferisci:
-import theme from './components/ui/themes/obsidian'; // 🌑 Obsidian (dark viola)
-// import theme from './components/ui/themes/default'; // ☀️ Default Apple (chiaro)
+import theme from './components/ui/themes/default'; // ☀️ Default Apple (chiaro)
+//import theme from './components/ui/themes/obsidian'; // 🌑 Obsidian (dark viola)
 // import theme from './components/ui/themes/neon';     // 💚 Neon (verde glow)
 // import theme from './components/ui/themes/stone';    // 🪨 Stone (marrone caldo)
 
